b506cc735d482a22923488f48e7e653e208633de
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-12-16 18:39:32 +0000

    third commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
-import react from "react";
+import React from "react";
 
 
 const App = () =>{
     return(
         <div id="main">
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            <p>
+                Now I can render any React component on any DOM node I want using ReactDOM.render
+            </p>
         </div>
     )
 }
